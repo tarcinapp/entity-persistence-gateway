2751665359fbbc54697927ef05441f8d5b1ae0ca
Kürşat Tokpınar <kursattokpinar@gmail.com>
2020-12-12 15:50:24 +0300

    Add JsonInclude for base record

--- src/main/java/com/tarcinapp/entitypersistencegateway/dto/AnyRecordBase.java
+++ src/main/java/com/tarcinapp/entitypersistencegateway/dto/AnyRecordBase.java
@@ -2,7 +2,10 @@ package com.tarcinapp.entitypersistencegateway.dto;
 
 import java.time.ZonedDateTime;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+@JsonInclude(Include.NON_NULL)
 public class AnyRecordBase {
     
     private String id;
