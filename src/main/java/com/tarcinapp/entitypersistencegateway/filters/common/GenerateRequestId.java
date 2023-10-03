9cd7971df383246ba01d2a73e74fb0cf9b39ece2
Kürşat Tokpunar <kursattokpinar@gmail.com>
2023-10-03 14:08:35 +0300

    Implement request id generation filter

--- src/main/java/com/tarcinapp/entitypersistencegateway/filters/common/GenerateRequestId.java
+++ src/main/java/com/tarcinapp/entitypersistencegateway/filters/common/GenerateRequestId.java
@@ -8,10 +8,13 @@ import java.util.Date;
 import com.tarcinapp.entitypersistencegateway.GatewaySecurityContext;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -20,39 +23,37 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class GenerateRequestId  extends AbstractGatewayFilterFactory<GenerateRequestId.Config>  {
+public class GenerateRequestId implements GlobalFilter {
 
     @Value("${app.requestHeaders.requestId}")
     private String requestIdHeader;
 
-    @Value("${app.requestIdPrefix}")
+    @Value("${app.shortcode}")
     private String requestIdPrefix;
 
     private final static String GATEWAY_SECURITY_CONTEXT_ATTR = "GatewaySecurityContext";
     private final static String REQUEST_ID_ATTR = "RequestId";
 
+    private Logger logger = LogManager.getLogger(GenerateRequestId.class);
+
     public GenerateRequestId() {
-        super(Config.class);
     }
 
-    @Override
-    public GatewayFilter apply(Config config) {
-        
-        // implement in seperate method in order to reduce nesting
-        return (exchange, chain) -> this.filter(exchange, chain);
-    }
-    
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         GatewaySecurityContext gc = exchange.getAttribute(GATEWAY_SECURITY_CONTEXT_ATTR);
 
         StringBuilder sb = new StringBuilder();
 
         // create date part
-        Date date = Calendar.getInstance().getTime();  
-        DateFormat dateFormat = new SimpleDateFormat("yyyyMMddhHHmmss");  
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmssSSS");
         String datePart = dateFormat.format(date);
 
-        // create auth party part
+        /**
+         * Authorized Party is the 'azp' field in the JSON token.
+         * The "azp" claim is used to indicate the party (usually a client application)
+         * to which the issuer of the token has granted authorization.
+         */
         String authParty = gc.getAuthParty() != null ? gc.getAuthParty() : "unknown";
 
         // create random part
@@ -60,22 +61,25 @@ public class GenerateRequestId  extends AbstractGatewayFilterFactory<GenerateReq
 
         sb.append(requestIdPrefix.toUpperCase());
         sb.append("-");
-        sb.append(datePart);
-        sb.append("-");
         sb.append(authParty.toUpperCase());
+        sb.append("-");
+        sb.append(datePart);
         sb.append("-");
         sb.append(random.toUpperCase());
 
         String requestId = sb.toString();
 
+
         ServerHttpRequest request = exchange.getRequest()
-            .mutate()
-            .header(requestIdHeader, requestId)
-            .build();
+                .mutate()
+                .header(requestIdHeader, requestId)
+                .build();
 
         exchange.getAttributes().put(REQUEST_ID_ATTR, requestId);
 
         ThreadContext.put(REQUEST_ID_ATTR, requestId);
+
+        logger.debug("Request id is generated: " + requestId);
 
         return chain.filter(exchange.mutate().request(request).build());
     }
