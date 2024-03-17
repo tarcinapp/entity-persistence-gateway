17aee7b472daba9b0b8367634f6bf3c6b6614ea3
kursattokpinar <kursattokpinar@gmail.com>
2024-03-17 15:41:09 +0000

    Implement add sets to query

--- src/main/java/com/tarcinapp/entitypersistencegateway/filters/common/AddSetsToQuery.java
+++ src/main/java/com/tarcinapp/entitypersistencegateway/filters/common/AddSetsToQuery.java
@@ -55,129 +55,147 @@ import org.springframework.web.util.UriComponentsBuilder;
  */
 @Component
 public class AddSetsToQuery
-        extends AbstractGatewayFilterFactory<AddSetsToQuery.Config> {
+                extends AbstractGatewayFilterFactory<AddSetsToQuery.Config> {
 
-    private Logger logger = LogManager.getLogger(AddSetsToQuery.class);
+        private Logger logger = LogManager.getLogger(AddSetsToQuery.class);
 
-    private final static String GATEWAY_SECURITY_CONTEXT_ATTR = "GatewaySecurityContext";
+        private final static String GATEWAY_SECURITY_CONTEXT_ATTR = "GatewaySecurityContext";
 
-    @Value("${app.shortcode:#{tarcinapp}}")
-    private String appShortcode;
+        @Value("${app.shortcode:#{tarcinapp}}")
+        private String appShortcode;
 
-    public AddSetsToQuery() {
-        super(Config.class);
-    }
+        public AddSetsToQuery() {
+                super(Config.class);
+        }
 
-    @Override
-    public GatewayFilter apply(Config config) {
+        @Override
+        public GatewayFilter apply(Config config) {
 
-        return (exchange, chain) -> {
+                return (exchange, chain) -> {
 
-            logger.debug("AddSetsToQuery filter is started.");
+                        logger.debug("AddSetsToQuery filter is started.");
 
-            GatewaySecurityContext gc = (GatewaySecurityContext) exchange.getAttributes()
-                    .get(GATEWAY_SECURITY_CONTEXT_ATTR);
-            ArrayList<String> roles = gc.getRoles();
-            String userId = gc.getAuthSubject();
-            ArrayList<String> groups = gc.getGroups();
+                        GatewaySecurityContext gc = (GatewaySecurityContext) exchange.getAttributes()
+                                        .get(GATEWAY_SECURITY_CONTEXT_ATTR);
+                        ArrayList<String> roles = gc.getRoles();
+                        String userId = gc.getAuthSubject();
+                        ArrayList<String> groups = gc.getGroups();
+                        String recordType = config.getRecordType();
 
-            if (roles == null) {
-                logger.debug("Authentication information not found. Exiting filter without any modification.");
-                return chain.filter(exchange);
-            }
+                        if (roles == null) {
+                                logger.debug("Authentication information not found. Exiting filter without any modification.");
+                                return chain.filter(exchange);
+                        }
 
-            logger.debug("User roles are: " + roles);
+                        logger.debug("User roles are: " + roles);
 
-            /**
-             * If user role is any of the following, we do not need to add sets
-             */
-            Stream<String> prefixedRolesStream = Stream.of(
-                    appShortcode + ".admin",
-                    appShortcode + ".editor",
-                    appShortcode + ".records.admin",
-                    appShortcode + ".records.find.admin",
-                    appShortcode + ".records.editor",
-                    appShortcode + ".records.find.editor",
-                    appShortcode + ".entities.find.admin",
-                    appShortcode + ".entities.find.editor");
+                        /**
+                         * If user role is any of the following, we do not need to add sets
+                         */
+                        Stream<String> prefixedRolesStream = Stream.of(
+                                        appShortcode + ".admin",
+                                        appShortcode + ".editor",
+                                        appShortcode + ".records.admin",
+                                        appShortcode + ".records.find.admin",
+                                        appShortcode + ".records.editor",
+                                        appShortcode + ".records.find.editor");
 
-            /**
-             * TODO: Should I ask this to PEP?
-             * This filter only applies when user has lower authority then editor user.
-             */
-            if (prefixedRolesStream.anyMatch(roles::contains)) {
+                        if (recordType != null) {
+                                prefixedRolesStream = Stream.concat(prefixedRolesStream,
+                                                Stream.of(
+                                                                appShortcode + "." + recordType + ".find.admin",
+                                                                appShortcode + "." + recordType + ".find.editor"));
+                        }
 
-                logger.debug(
-                        "No need to limit response items for these roles. Exiting filter withouth any modification.");
-                return chain.filter(exchange);
-            }
+                        /**
+                         * TODO: Should I ask this to PEP?
+                         * This filter only applies when user has lower authority then editor user.
+                         */
+                        if (prefixedRolesStream.anyMatch(roles::contains)) {
 
-            URI uri = exchange.getRequest().getURI();
-            logger.debug("Original URI: " + uri);
+                                logger.debug(
+                                                "No need to limit response items for these roles. Exiting filter withouth any modification.");
+                                return chain.filter(exchange);
+                        }
 
-            List<NameValuePair> query = URLEncodedUtils.parse(uri, Charset.forName("UTF-8"));
+                        URI uri = exchange.getRequest().getURI();
+                        logger.debug("Original URI: " + uri);
 
-            /**
-             * If user asks for specific sets, make sure they are valid under enforced
-             * scope.
-             * To that end, move all set queries under the 'and' clause and combine with
-             * enforced sets.
-             */
-            List<NameValuePair> newQuery = query.stream()
-                    .map(nvp -> {
-                        String newName = nvp.getName()
-                                .replace("set[", "set[and][0][");
-                        return new BasicNameValuePair(newName, nvp.getValue());
-                    })
-                    .collect(Collectors.toList());
+                        List<NameValuePair> query = URLEncodedUtils.parse(uri, Charset.forName("UTF-8"));
 
-            /**
-             * Following set is added in order to reduce the scope of the query.
-             * Users with low authority can only see the public and active records with
-             * their own active and pending records.
-             * This is exactly what 'audience' set does.
-             * 
-             * Note: If a record has user's user id in its ownerUsers array, then this
-             * record belongs to that user.
-             * In addition to that, if user's group id presents in record's ownerGroups
-             * array and it's visibility is protected or public (not private) this record is also belongs to that user too.
-             * In addition if user's id is in viewerUsers, then user is allowed to see no matter what is the visibility.
-             * In addition if user's group id is in viewerGroups, then user is allowed to see as long as visibility is protected or public (not private)
-             */
+                        /**
+                         * If user asks for specific sets, make sure they are valid under enforced
+                         * scope.
+                         * To that end, move all set queries under the 'and' clause and combine with
+                         * enforced sets.
+                         */
+                        List<NameValuePair> newQuery = query.stream()
+                                        .map(nvp -> {
+                                                String newName = nvp.getName()
+                                                                .replace("set[", "set[and][0][");
+                                                return new BasicNameValuePair(newName, nvp.getValue());
+                                        })
+                                        .collect(Collectors.toList());
 
-            // audience set requires user id and groups in following format
-            // [userId1,userId2][group1,group2]
-            String groupsStr = groups.stream().collect(Collectors.joining(","));
-            newQuery.add(new BasicNameValuePair("set[and][1][audience][userIds]", userId));
-            newQuery.add(new BasicNameValuePair("set[and][1][audience][groupIds]", groupsStr));
+                        /**
+                         * Following set is added in order to reduce the scope of the query.
+                         * Users with low authority can only see the public and active records with
+                         * their own active and pending records.
+                         * This is exactly what 'audience' set does.
+                         * 
+                         * Note: If a record has user's user id in its ownerUsers array, then this
+                         * record belongs to that user.
+                         * In addition to that, if user's group id presents in record's ownerGroups
+                         * array and it's visibility is protected or public (not private) this record is
+                         * also belongs to that user too.
+                         * In addition if user's id is in viewerUsers, then user is allowed to see no
+                         * matter what is the visibility.
+                         * In addition if user's group id is in viewerGroups, then user is allowed to
+                         * see as long as visibility is protected or public (not private)
+                         */
 
-            // as we built new query string, now we can go ahead and change the query from
-            // the original request
-            ServerWebExchange modifiedExchange = exchange.mutate()
-                    .request(originalRequest -> {
+                        // audience set requires user id and groups in following format
+                        // [userId1,userId2][group1,group2]
+                        String groupsStr = groups.stream().collect(Collectors.joining(","));
+                        newQuery.add(new BasicNameValuePair("set[and][1][audience][userIds]", userId));
+                        newQuery.add(new BasicNameValuePair("set[and][1][audience][groupIds]", groupsStr));
 
-                        String newQueryStr = newQuery.stream()
-                                .map(v -> v.getName() + "=" + v.getValue())
-                                .collect(Collectors.joining("&"));
+                        // as we built new query string, now we can go ahead and change the query from
+                        // the original request
+                        ServerWebExchange modifiedExchange = exchange.mutate()
+                                        .request(originalRequest -> {
 
-                        URI newUri = UriComponentsBuilder.fromUri(uri)
-                                .replaceQuery(newQueryStr)
-                                .encode()
-                                .build()
-                                .toUri();
+                                                String newQueryStr = newQuery.stream()
+                                                                .map(v -> v.getName() + "=" + v.getValue())
+                                                                .collect(Collectors.joining("&"));
 
-                        logger.debug("New URI " + newUri);
+                                                URI newUri = UriComponentsBuilder.fromUri(uri)
+                                                                .replaceQuery(newQueryStr)
+                                                                .encode()
+                                                                .build()
+                                                                .toUri();
 
-                        originalRequest
-                                .uri(newUri);
-                    })
-                    .build();
+                                                logger.debug("New URI " + newUri);
 
-            return chain.filter(modifiedExchange);
-        };
-    }
+                                                originalRequest
+                                                                .uri(newUri);
+                                        })
+                                        .build();
 
-    public static class Config {
+                        return chain.filter(modifiedExchange);
+                };
+        }
 
-    }
+        public static class Config {
+
+                private String recordType;
+
+                public String getRecordType() {
+                        return this.recordType;
+                }
+
+                public void setRecordType(String recordType) {
+                        this.recordType = recordType;
+                }
+        }
 }
