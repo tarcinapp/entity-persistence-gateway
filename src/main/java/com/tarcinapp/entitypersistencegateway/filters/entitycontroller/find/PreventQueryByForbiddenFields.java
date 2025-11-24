f70c2b5da97161120d3c0553faa449baff8cc5d4
Kürşat Tokpınar <kursattokpinar@gmail.com>
2025-11-24 15:02:46 +0300

    Remove unused import of AbstractGatewayFilterFactory in PreventQueryByForbiddenFields filter

--- src/main/java/com/tarcinapp/entitypersistencegateway/filters/entitycontroller/find/PreventQueryByForbiddenFields.java
+++ src/main/java/com/tarcinapp/entitypersistencegateway/filters/entitycontroller/find/PreventQueryByForbiddenFields.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
