efca163b5f9c52aa1c58ce7db6d70874fefdec90
Kürşat Tokpınar <kursattokpinar@gmail.com>
2025-12-20 22:53:43 +0300

    refactor: Remove unused import for Value annotation in CheckIfRouteEnabled filter

--- src/main/java/com/tarcinapp/entitypersistencegateway/filters/common/request/CheckIfRouteEnabled.java
+++ src/main/java/com/tarcinapp/entitypersistencegateway/filters/common/request/CheckIfRouteEnabled.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.Route;
